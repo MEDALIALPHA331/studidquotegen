135255dd11c3962a290d8abaa51c257080e84153
dali <medalikhaled331@gmail.com>
2023-07-03 19:53:29 +0100

    fix theme

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import ThemeSwitch from "./theme";
 
 function Nav() {
   return (
-    <nav className="flex min-h-[20%] items-start justify-around">
-      <ul className="darkshadow-slate-100 flex items-center justify-center gap-1 shadow-lg shadow-black">
+    <nav className="flex min-h-[20%] items-start justify-around rounded-md px-4 py-2">
+      <ul className="flex items-center justify-center gap-1 shadow-lg shadow-black dark:shadow-slate-100">
         <li>
           <a href="" target="_blank" rel="noopener noreferrer">
             g
